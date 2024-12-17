e895d49301442d6d920184c48f29498b2670d659
Tim Barton <timothyebarton@gmail.com>
2024-12-17 15:02:35 +1000

    dec-7 part 2 working solution

--- cmd/dec-7/main.go
+++ cmd/dec-7/main.go
@@ -13,11 +13,13 @@ type Operator int
 const (
 	Add Operator = iota
 	Multiply
+	Concat
 )
 
 var stateName = map[Operator]string{
 	Add:      "+",
 	Multiply: "*",
+	Concat:   "||",
 }
 
 func (pt Operator) String() string {
@@ -55,6 +57,36 @@ func buildOperatorList(depth int) <-chan []Operator {
 	return output
 }
 
+func buildOperatorList2(depth int) <-chan []Operator {
+	output := make(chan []Operator)
+
+	go func() {
+		var recursion func([]Operator, int)
+
+		recursion = func(current []Operator, depth int) {
+			if depth == 1 {
+				newList := append([]Operator{}, current...)
+				output <- append(newList, Add)
+				newList = append([]Operator{}, current...)
+				output <- append(newList, Multiply)
+				newList = append([]Operator{}, current...)
+				output <- append(newList, Concat)
+			} else {
+				newList := append([]Operator{}, current...)
+				recursion(append(newList, Add), depth-1)
+				newList = append([]Operator{}, current...)
+				recursion(append(newList, Multiply), depth-1)
+				newList = append([]Operator{}, current...)
+				recursion(append(newList, Concat), depth-1)
+			}
+		}
+		recursion([]Operator{}, depth)
+		close(output)
+	}()
+
+	return output
+}
+
 func (c Calibration) Assess() bool {
 	assessments := buildOperatorList(len(c.inputs) - 1)
 
@@ -66,6 +98,29 @@ func (c Calibration) Assess() bool {
 				localAnswer += c.inputs[i]
 			case Multiply:
 				localAnswer *= c.inputs[i]
+			}
+		}
+		if localAnswer == c.answer {
+			//fmt.Printf("Answer: %d, inputs: %v, Operators: %v\n", c.answer, c.inputs, opList)
+			return true
+		}
+	}
+	return false
+}
+
+func (c Calibration) Assess2() bool {
+	assessments := buildOperatorList2(len(c.inputs) - 1)
+
+	for opList := range assessments {
+		localAnswer := c.inputs[0]
+		for i := 1; i < len(c.inputs); i++ {
+			switch opList[i-1] {
+			case Add:
+				localAnswer += c.inputs[i]
+			case Multiply:
+				localAnswer *= c.inputs[i]
+			case Concat:
+				localAnswer, _ = strconv.Atoi(strconv.Itoa(localAnswer) + strconv.Itoa(c.inputs[i]))
 			}
 		}
 		if localAnswer == c.answer {
@@ -113,12 +168,26 @@ func AssessCalibrationData(input string) int {
 	return output
 }
 
+func AssessCalibrationData2(input string) int {
+	calibrations := parseInput(input)
+	output := 0
+	for i := range calibrations {
+		correct := calibrations[i].Assess2()
+		if correct {
+			output += calibrations[i].answer
+		}
+	}
+	return output
+}
+
 func main() {
 
 	input, _ := os.Open("input")
 	data, _ := io.ReadAll(input)
 	result := AssessCalibrationData(string(data))
+	result2 := AssessCalibrationData2(string(data))
 
 	fmt.Println(result)
+	fmt.Println(result2)
 
 }
--- cmd/dec-7/main_test.go
+++ cmd/dec-7/main_test.go
@@ -38,3 +38,23 @@ func TestAssessCalibration(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestAssessCalibration2(t *testing.T) {
+	testData := "190: 10 19\n" +
+		"3267: 81 40 27\n" +
+		"83: 17 5\n" +
+		"156: 15 6\n" +
+		"7290: 6 8 6 15\n" +
+		"161011: 16 10 13\n" +
+		"192: 17 8 14\n" +
+		"21037: 9 7 18 13\n" +
+		"292: 11 6 16 20\n"
+
+	expectedResult := 11387
+	result := AssessCalibrationData2(testData)
+
+	if expectedResult != result {
+		fmt.Printf("Expected: %d, Returned: %d\n", expectedResult, result)
+		t.Fail()
+	}
+}
